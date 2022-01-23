4f5d7ef513376976e481e4f48add61dce167978c
abdulnazifSource <abdulnazifmail@gmail.com>
2022-01-23 15:24:26 +0100

    added new buttonfever library

--- IoT_weighing_scale.h
+++ IoT_weighing_scale.h
@@ -3,4 +3,6 @@
 #include <Arduino.h>
 #include "src/LiquidCrystal_I2C/LiquidCrystal_I2C.h"
 #include "src/HX711/HX711.h"
+#include "src/ButtonFever/BfButton.h"
+#include "src/ButtonFever/BfButtonManager.h"
 #endif
--- IoT_weighing_scale.ino
+++ IoT_weighing_scale.ino
@@ -1,62 +1,146 @@
 #include "IoT_weighing_scale.h"
 
-#define DOUT  26
-#define CLK  25
-HX711 scale(DOUT, CLK);
+uint8_t dataPin = 26;
+uint8_t clockPin = 25;
+
+
+HX711 scale;
+
 // set the LCD number of columns and rows
 int lcdColumns = 16;
 int lcdRows = 2;
 
+
+float calibration_factor = 832.10; // for me this value works just perfect 419640 
+float weight, price;
+char blank[] = "         ";
+
 // set LCD address, number of columns and rows
 // if you don't know your display address, run an I2C scanner sketch
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
- 
-float weight; 
-float calibration_factor = 419640; // for me this value works just perfect 419640 
-void setup() {
-  Serial.begin(9600);
+
+int btnPin = 14;
+
+
+
+BfButtonManager btnManager(btnPin, 4);
+BfButton btn1(BfButton::ANALOG_BUTTON_ARRAY, 0, false, HIGH);
+BfButton btn2(BfButton::ANALOG_BUTTON_ARRAY, 1, false, HIGH);
+BfButton btn3(BfButton::ANALOG_BUTTON_ARRAY, 2, false, HIGH);
+BfButton btn4(BfButton::ANALOG_BUTTON_ARRAY, 3, false, HIGH);
+
+void pressHandler(BfButton *btn, BfButton::press_pattern_t pattern)
+{
+  Serial.print(btn->getID());
+  switch (pattern)
+  {
+  case BfButton::SINGLE_PRESS:
+    Serial.println(" pressed.");
+      lcd.setCursor(7, 1);
+      lcd.print(blank);
+      lcd.setCursor(7, 1);
+      lcd.print("pressed.");
+    break;
+  case BfButton::DOUBLE_PRESS:
+    Serial.println(" double pressed.");
+    lcd.setCursor(7, 1);
+    lcd.print(blank);
+    lcd.setCursor(7, 1);
+    lcd.print("double P.");
+    break;
+  case BfButton::LONG_PRESS:
+    Serial.println(" long pressed.");
+    lcd.setCursor(7, 1);
+    lcd.print(blank);
+    lcd.setCursor(7, 1);
+    lcd.print("long P.");
+    break;
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(btnPin, INPUT_PULLDOWN);
+  /*
+  Serial.println(__FILE__);
+  Serial.print("LIBRARY VERSION: ");
+  Serial.println(HX711_LIB_VERSION);
+  Serial.println();
+  */ 
+  //integrate event handlers to buttons and add buttons to event manager
+  btn1.onPress(pressHandler)
+      .onDoublePress(pressHandler)     // default timeout
+      .onPressFor(pressHandler, 1000); // custom timeout for 1 second
+  btnManager.addButton(&btn1, 3000, 3150);
+  btn2.onPress(pressHandler)
+      .onDoublePress(pressHandler)     // default timeout
+      .onPressFor(pressHandler, 1000); // custom timeout for 1 second
+  btnManager.addButton(&btn2, 2190, 2350);
+  btn3.onPress(pressHandler)
+      .onDoublePress(pressHandler)     // default timeout
+      .onPressFor(pressHandler, 1000); // custom timeout for 1 second
+  btnManager.addButton(&btn3, 1390, 1550);
+  btn4.onPress(pressHandler)
+      .onDoublePress(pressHandler)     // default timeout
+      .onPressFor(pressHandler, 1000); // custom timeout for 1 second
+  btnManager.addButton(&btn4, 600, 750);
+  btnManager.begin();
+  scale.begin(dataPin, clockPin);
   // initialize LCD
-  lcd.init();
+  lcd.begin();
   // turn on LCD backlight                      
   lcd.backlight();
-  Serial.println("HX711 calibration sketch");
-  Serial.println("Remove all weight from scale");
-  Serial.println("After readings begin, place known weight on scale");
-  Serial.println("Press + or a to increase calibration factor");
-  Serial.println("Press - or z to decrease calibration factor");
-  scale.set_scale();
-  scale.tare(); //Reset the scale to 0
-  long zero_factor = scale.read_average(); //Get a baseline reading
-  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
-  Serial.println(zero_factor);
-}
-void loop() {
-  scale.set_scale(calibration_factor); //Adjust to this calibration factor
-  weight = scale.get_units(1);
-  //Serial.print(scale.get_units(), 2);
- // Serial.print(" lbs"); //Change this to kg and re-adjust the calibration factor if you follow SI units like a sane person
+  /*
+  Serial.print("UNITS: ");
+  Serial.println(scale.get_units(10));
+
+  Serial.println("\nEmpty the scale, press a key to continue");
+  while(!Serial.available());
+  while(Serial.available()) Serial.read();
+
+  scale.tare();
+  Serial.print("UNITS: ");
+  Serial.println(scale.get_units(10));
+
+
+  Serial.println("\nPut 1000 gr in the scale, press a key to continue");
+  while(!Serial.available());
+  while(Serial.available()) Serial.read();
+
+  scale.calibrate_scale(1000, 5);
+  Serial.print("UNITS: ");
+  Serial.println(scale.get_units(10));
+
+  Serial.println("\nScale is calibrated, press a key to continue");
+  while(!Serial.available());
+  while(Serial.available()) Serial.read();
+
   
-   // set cursor to first column, first row
+  
+  scale.set_unit_price(20);  // we only have one price
   lcd.setCursor(0, 0);
-  // print message
+  lcd.print("UNITS: ");
+  lcd.setCursor(0, 1);
+  lcd.print(": ");
+  */
+  scale.tare();
+  scale.set_scale(calibration_factor);
   lcd.clear();
-  lcd.print("gram: ");
+}
+
+
+void loop()
+{
+
+  btnManager.printReading(14);
+  btnManager.loop();
+  
+  weight = scale.get_units(5);
+  price = 20;
+  
+  lcd.setCursor(0, 0);
   lcd.print(weight);
-  lcd.print(" g");
-  
-  // clears the display to print new message
-  
-  // set cursor to first column, second row
-  lcd.setCursor(0,1);
-  lcd.print("Calib: ");
-  lcd.print(calibration_factor);
-  if(Serial.available())
-  {
-    char temp = Serial.read();
-    if(temp == '+' || temp == 'a')
-      calibration_factor += 10;
-    else if(temp == '-' || temp == 'z')
-      calibration_factor -= 10;
-  }
-  delay(1000);
+  lcd.setCursor(0, 1);
+  lcd.print(weight * price);
 }
