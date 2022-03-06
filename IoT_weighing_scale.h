7da670fcee362ad1ec939b50ced26db5b59bb06c
abdulnazifSource <abdulnazifmail@gmail.com>
2022-03-06 22:29:45 +0100

    added 3 libraries TinyGSM, SSLClient, and ArduinoHttpClient to enable secure https protocol via SSLClient

--- IoT_weighing_scale.h
+++ IoT_weighing_scale.h
@@ -6,7 +6,15 @@
 #include "src/HX711_ADC/HX711_ADC.h"
 #include "src/ButtonFever/BfButton.h"
 #include "src/ButtonFever/BfButtonManager.h"
-#include "src/Adafruit_FONA/Adafruit_FONA.h"
+//Please enter your CA certificate in ca_cert.h
+#include "src/ca/ca_cert.h"
+#include "src/SSLClient/SSLClient.h"
+#include "src/ArduinoHttpClient/ArduinoHttpClient.h"
+// Configure TinyGSM library
+#define TINY_GSM_MODEM_SIM800   // Modem is SIM800
+#define TINY_GSM_RX_BUFFER 1024 // Set RX buffer to 1Kb
+// Include after TinyGSM definitions
+#include "src/TinyGsmClient/TinyGsmClient.h"
 //#include "soc/rtc.h"
 #if defined(ESP8266)|| defined(ESP32) || defined(AVR)
 #include "EEPROM.h"
--- IoT_weighing_scale.ino
+++ IoT_weighing_scale.ino
@@ -13,10 +13,35 @@ unsigned long t = 0;
 int simRXPin = 16;
 int simTXPin = 17;
 int simResetPin = 18;
+
+#define MODEM_UART_BAUD 9600
+#define MODEM_RST 18
+#define MODEM_TX 17
+#define MODEM_RX 16
+
+
 // create an instance for serial communication to sim800l module
-HardwareSerial sim800l(2);
-// create an of adafruit_FONA library class
-Adafruit_FONA gsm = Adafruit_FONA(simResetPin);
+//HardwareSerial sim800l(2);
+
+// Set serial for debug console (to the Serial Monitor)
+#define SerialMon Serial
+// Set serial for AT commands (to the SIM800 module)
+#define SerialAT Serial2
+
+const char apn[] = "web.gprs.mtnnigeria.net";       // Your APN
+const char gprs_user[] = "web"; // User
+const char gprs_pass[] = "web"; // Password
+const char simPIN[] = "";    // SIM card PIN code, if any
+
+const char hostname[] = "https://reqres.in"; // www.howsmyssl.com
+const char resource[] = "/api/users?page=2";
+int port = 443;
+
+// Layers stack
+TinyGsm sim_modem(SerialAT);
+TinyGsmClient gsm_transpor_layer(sim_modem);
+SSLClient secure_presentation_layer(&gsm_transpor_layer);
+HttpClient http_client = HttpClient(secure_presentation_layer, hostname, port);
 
 // buzzer
 int buzzerPin = 32;
@@ -79,37 +104,109 @@ void pressHandler(BfButton *btn, BfButton::press_pattern_t pattern)
   }
 }
 
-void smsHandler(BfButton *btn, BfButton::press_pattern_t pattern)
+void httpRequestHandler(BfButton *btn, BfButton::press_pattern_t pattern)
 {
   Serial.print(btn->getID());
   if (pattern == BfButton::SINGLE_PRESS) {
     Serial.println(" button pressed.");
-    char sendto[21] = "09069025946";
-    char message[141] = "Hello from digital weighing scale";
-    gsm.sendSMS(sendto, message);
+    // HTTP Test
+    if (sim_modem.isGprsConnected()) {
+      Serial.println("");
+      Serial.println("Making GET request");
+      http_client.get(resource);
+
+      int status_code = http_client.responseStatusCode();
+      String response = http_client.responseBody();
+
+      Serial.print("Status code: ");
+      Serial.println(status_code);
+      Serial.print("Response: ");
+      Serial.println(response);
+
+      http_client.stop();
+    } else {
+      Serial.println("...not connected");
+    }
+
+    // Disconnect GPRS
+    //sim_modem.gprsDisconnect();
+    //SerialMon.println("GPRS disconnected");
     delay(1000);
-    Serial.println("message sent.");
   }
 }
+
 
 void setup()
 {
   Serial.begin(9600);
-  sim800l.begin(9600);
+  SerialAT.begin(115200);
+  lcd.begin();// initialize LCD
+  lcd.backlight();// turn on LCD backlight
+  lcd.clear();
+  lcd.print("Starting...");
+  
+  //Add CA Certificate
+  secure_presentation_layer.setCACert(root_ca);
+  
+  SerialMon.print("Initializing modem...");
+  if (!sim_modem.init()) {
+    SerialMon.print(" fail... restarting modem...");
+    // Restart takes quite some time
+    // Use modem.init() if you don't need the complete restart
+    if (!sim_modem.restart()) {
+      SerialMon.println(" fail... even after restart");
+    } else {
+      SerialMon.println(" OK");
+    }
+  } else {
+    SerialMon.println(" OK");
+  }
+
+  // Unlock your SIM card with a PIN if needed
+  if (strlen(simPIN) && sim_modem.getSimStatus() != 3) {
+    sim_modem.simUnlock(simPIN);
+  }
+
+  // Wait for network availability
+  SerialMon.print("Waiting for network...");
+  if (sim_modem.waitForNetwork(240000L)) {
+    SerialMon.println(" OK");
+    // Connect to the GPRS network
+    SerialMon.print("Connecting to network...");
+    if (sim_modem.isNetworkConnected()) {
+      SerialMon.println(" OK");
+      // Connect to APN
+      SerialMon.print(F("Connecting to APN: "));
+      SerialMon.print(apn);
+      if (sim_modem.gprsConnect(apn, gprs_user, gprs_pass)) {
+        SerialMon.println(" OK");
+      } else {
+        SerialMon.println(" fail");
+      }
+    } else {
+      SerialMon.println(" fail");
+    }
+  } else {
+    SerialMon.println(" fail");
+  }
+  
+  // More info..
+  Serial.println("");
+  IPAddress local = sim_modem.localIP();
+  Serial.println("Local IP: " + String(local));
+  int csq = sim_modem.getSignalQuality();
+  Serial.println("Signal quality: " + String(csq));
+
   //rtc_clk_cpu_freq_set(RTC_CPU_FREQ_80M);
   delay(10);
   Serial.println();
   Serial.println("Starting...");
 
   
-  if(! gsm.begin(sim800l)) {
-    Serial.println("Could not find sim800l");
-  }
-
   pinMode(buzzerPin, OUTPUT);
   pinMode(processPin, OUTPUT);
-  digitalWrite(buzzerPin, HIGH);
-  digitalWrite(processPin, HIGH);
+  digitalWrite(buzzerPin, LOW);
+  digitalWrite(processPin, LOW);
   LoadCell.begin();
   //LoadCell.setReverseOutput(); //uncomment to turn a negative output value to positive
   float calibrationValue; // calibration value (see example file "Calibration.ino")
@@ -124,7 +221,7 @@ void setup()
   LoadCell.setCalFactor(calibrationValue); // set calibration value (float)
   
   //integrate event handlers to buttons and add buttons to event manager
-  btn1.onPress(smsHandler)
+  btn1.onPress(httpRequestHandler)
       .onDoublePress(pressHandler)     // default timeout
       .onPressFor(pressHandler, 1000); // custom timeout for 1 second
   btnManager.addButton(&btn1, 180, 230);
@@ -142,45 +239,8 @@ void setup()
   btnManager.addButton(&btn4, 900, 1100);
   btnManager.begin();
   
-  lcd.begin();// initialize LCD
-  lcd.backlight();// turn on LCD backlight
-  lcd.clear();
   Serial.println("Startup is complete");
 
-  /*
-  Serial.print("UNITS: ");
-  Serial.println(scale.get_units(10));
-
-  Serial.println("\nEmpty the scale, press a key to continue");
-  while(!Serial.available());
-  while(Serial.available()) Serial.read();
-
-  scale.tare();
-  Serial.print("UNITS: ");
-  Serial.println(scale.get_units(10));
-
-
-  Serial.println("\nPut 1000 gr in the scale, press a key to continue");
-  while(!Serial.available());
-  while(Serial.available()) Serial.read();
-
-  scale.calibrate_scale(1000, 5);
-  Serial.print("UNITS: ");
-  Serial.println(scale.get_units(10));
-
-  Serial.println("\nScale is calibrated, press a key to continue");
-  while(!Serial.available());
-  while(Serial.available()) Serial.read();
-
-  
-  
-  scale.set_unit_price(20);  // we only have one price
-  lcd.setCursor(0, 0);
-  lcd.print("UNITS: ");
-  lcd.setCursor(0, 1);
-  lcd.print(": ");
-  */
-  
 }
 
 
@@ -191,16 +251,16 @@ void loop()
   //if (z > 100) lcd.print(z);
   //btnManager.printReading(btnPin);
   btnManager.loop();
-
+  
   // setup serial transmission between serial 0 (serial monitor) and serial 2 (sim module)
   while(Serial.available()) {
-    delay(1);
-    sim800l.write(Serial.read());
+    delay(1); 
+    SerialAT.write(SerialMon.read());
   }
-  while(sim800l.available()) {
-    Serial.write(sim800l.read());
+  while(SerialAT.available()) {
+    SerialMon.write(SerialAT.read());
   }
-
+  /**/
   static boolean newDataReady = 0;
   const int serialPrintInterval = 100; //increase value to slow down serial print activity
 
@@ -211,8 +271,8 @@ void loop()
   if (newDataReady) {
     if (millis() > t + serialPrintInterval) {
       float i = LoadCell.getData();
-      //Serial.print("Load_cell output val: ");
-      //Serial.println(i);
+      SerialMon.print("Load_cell output val: ");
+      SerialMon.println(i);
       lcd.clear();
       price = i * 20;
       
